88b83d87bb76c3e8f324c7e10325cec280433ee1
Pablo Pazmino <elpablopazmino@gmail.com>
2020-10-03 10:25:34 -0500

    email

--- app/Http/Controllers/ContactUsController.php
+++ app/Http/Controllers/ContactUsController.php
@@ -2,8 +2,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Email
-use Redirect
+use Email;
+use Redirect;
 class ContactUsController extends BaseController {
 
 	public function contact_us(Request $request) {
